b6ce1b79cae2e0d1c04ee8cb40fc89e29ae96047
safasidhi <115776259+safasidhi@users.noreply.github.com>
2023-02-16 12:57:28 +0530

    Delete C03 directory

--- C03/pack_main/rectangle.py
+++
@@ -1,6 +0,0 @@
-def area_rect(l,b):
-    area=l*b
-    return area
-def peri_react(l,b):
-    peri=2*(l+b)
-    return peri
--- C03/packageDemo2.py
+++
@@ -1,8 +0,0 @@
-import pack_main.pack_sub.cuboid as pc
-
-print("Cuboid volume : ",pc.volume_cuboid(10,5,7))
-print("Cuboid perimeter : ",pc.peri_cuboid(10,5,7))
-
-from pack_main.pack_sub.sphere import surface_area_sphere
-
-print(surface_area_sphere(5))
